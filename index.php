3a47232b2c11e90c9d2af944195138fcd97d72d1
0903244 <133035272+0903244@users.noreply.github.com>
2023-05-10 01:31:29 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" alt="">
+        </div>
+        <nav class="topnav">
+            <ul>
+                <li>
+                    <a href="#">about</a>
+                </li>
+                <li>
+                    <a href="#">sample</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="mainvisual">
+
+    </div>
+
+    <section class="about">
+        <h2 class="sectiontitle">About</h2>
+        <div class="profile">
+            <div class="left">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/about.jpg" alt="">
+            </div>
+            <div class="right">
+                <h3>
+                    テキストテキスト
+                </h3>
+                <p>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="bicycle">
+        <h2 class="sectiontitle">sample</h2>
+        <div class="bicyclebox">
+            <div>
+                <div class="col3">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bicycle1.jpg" alt="">
+                </div>
+                <p>ここにテキストを入力します</p>
+            </div>
+            <div class="col3">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bicycle2.jpg" alt="">
+                <p>ここにテキストを入力します</p>
+            </div>
+            <div class="col3">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bicycle3.jpg" alt="">
+                <p>ここにテキストを入力します</p>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- <div class="top">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/エラー表示.png" alt="">
+    </div> -->
+</body>
+
+</html>
